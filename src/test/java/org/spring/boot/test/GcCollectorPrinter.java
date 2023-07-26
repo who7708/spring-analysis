e075195dfbf6834f9b6b5911dd3999f8d0daa95a
Chris <who7708@163.com>
2023-07-26 15:24:27 +0800

    测试打印GC收集器的名称

---
+++ src/test/java/org/spring/boot/test/GcCollectorPrinter.java
@@ -0,0 +1,24 @@
+package org.spring.boot.test;
+
+import java.lang.management.GarbageCollectorMXBean;
+import java.lang.management.ManagementFactory;
+import java.util.List;
+
+public class GcCollectorPrinter {
+    public static void main(String[] args) {
+        List<GarbageCollectorMXBean> beans = ManagementFactory.getGarbageCollectorMXBeans();
+        for (GarbageCollectorMXBean bean : beans) {
+            System.out.println(bean.getName());
+        }
+    }
+}
+// UseG1GC
+// UseConcMarkSweepGC || UseParNewGC
+// UseParallelGC || UseParallelOldGC 两个是相同的结果
+// UseSerialGC
+// java -verbose:gc -Xms20m -Xmx20m -Xmn10m -XX:+UseConcMarkSweepGC -XX:+PrintGCDetails -XX:+PrintGCTimeStamps -XX:+HeapDumpOnOutOfMemoryError GcCollectorPrinter
+// java -verbose:gc -Xms20m -Xmx20m -Xmn10m -XX:+UseParNewGC -XX:+PrintGCDetails -XX:+PrintGCTimeStamps -XX:+HeapDumpOnOutOfMemoryError GcCollectorPrinter
+// java -verbose:gc -Xms20m -Xmx20m -Xmn10m -XX:+UseParallelGC -XX:+PrintGCDetails -XX:+PrintGCTimeStamps -XX:+HeapDumpOnOutOfMemoryError GcCollectorPrinter
+// java -verbose:gc -Xms20m -Xmx20m -Xmn10m -XX:+UseParallelOldGC -XX:+PrintGCDetails -XX:+PrintGCTimeStamps -XX:+HeapDumpOnOutOfMemoryError GcCollectorPrinter
+// java -verbose:gc -Xms20m -Xmx20m -Xmn10m -XX:+UseSerialGC -XX:+PrintGCDetails -XX:+PrintGCTimeStamps -XX:+HeapDumpOnOutOfMemoryError GcCollectorPrinter
+// java -verbose:gc -Xms20m -Xmx20m -Xmn10m -XX:+UseG1GC -XX:+PrintGCDetails -XX:+PrintGCTimeStamps -XX:+HeapDumpOnOutOfMemoryError GcCollectorPrinter
