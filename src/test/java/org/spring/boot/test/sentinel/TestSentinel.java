a513edcb60e8a7705ae07d178d3832d8ce06dd2e
Chris <who7708@163.com>
2023-07-31 16:40:16 +0800

    测试接入sentinel

---
+++ src/test/java/org/spring/boot/test/sentinel/TestSentinel.java
@@ -0,0 +1,49 @@
+package org.spring.boot.test.sentinel;
+
+import com.alibaba.csp.sentinel.Entry;
+import com.alibaba.csp.sentinel.SphU;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+import com.alibaba.csp.sentinel.slots.block.RuleConstant;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowRule;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowRuleManager;
+import org.apache.commons.lang3.RandomUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author Chris
+ * @version 1.0.0
+ * @since 2023-07-31
+ */
+public class TestSentinel {
+    public static void main(String[] args) throws Exception {
+        // 配置规则.
+        initFlowRules();
+
+        while (true) {
+            // 1.5.0 版本开始可以直接利用 try-with-resources 特性
+            try (Entry entry = SphU.entry("HelloWorld")) {
+                // 被保护的逻辑
+                System.out.println("hello world");
+            } catch (BlockException ex) {
+                // 处理被流控的逻辑
+                System.out.println("blocked!");
+            }
+            int time = RandomUtils.nextInt(20, 50);
+            TimeUnit.MILLISECONDS.sleep(time);
+        }
+    }
+
+    private static void initFlowRules() {
+        List<FlowRule> rules = new ArrayList<>();
+        FlowRule rule = new FlowRule();
+        rule.setResource("HelloWorld");
+        rule.setGrade(RuleConstant.FLOW_GRADE_QPS);
+        // Set limit QPS to 20.
+        rule.setCount(20);
+        rules.add(rule);
+        FlowRuleManager.loadRules(rules);
+    }
+}
