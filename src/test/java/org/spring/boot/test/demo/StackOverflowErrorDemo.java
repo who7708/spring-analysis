c6207e73e9e106a6de6267c6d3b8c762546c814c
Chris <who7708@163.com>
2023-08-02 16:06:24 +0800

    测试java基础内存

---
+++ src/test/java/org/spring/boot/test/demo/StackOverflowErrorDemo.java
@@ -0,0 +1,11 @@
+package org.spring.boot.test.demo;
+
+public class StackOverflowErrorDemo {
+    public static void main(String[] args){
+        stackOverflowError();
+    }
+
+    private static void stackOverflowError() {
+        stackOverflowError();
+    }
+}
