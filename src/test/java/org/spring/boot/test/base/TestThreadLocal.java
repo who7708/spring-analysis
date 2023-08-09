84487f22c4a980cbae290dd42f779a8642c633bb
Chris <who7708@163.com>
2023-08-09 11:09:23 +0800

    测试ThreadLocal

---
+++ src/test/java/org/spring/boot/test/base/TestThreadLocal.java
@@ -0,0 +1,56 @@
+package org.spring.boot.test.base;
+
+import org.junit.Test;
+
+import java.lang.reflect.Method;
+
+/**
+ * @author Chris
+ * @version 1.0.0
+ * @since 2023-08-08
+ */
+public class TestThreadLocal {
+    @Test
+    public void test1() throws Exception {
+        System.out.println("===== test1 =====");
+        ThreadLocal<String> local = new ThreadLocal<>();
+        //设置值
+        local.set("hello word");
+        local.set("hello word2");
+        //获取刚刚设置的值
+        System.out.println(local.get());
+
+        for (int i = 0; i < 8; i++) {
+            int finalI = i;
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    local.set("Hello_" + finalI);
+                }
+            }).start();
+        }
+
+    }
+
+    @Test
+    public void testNextHashCode() throws Exception {
+        System.out.println("===== test2 =====");
+        ThreadLocal<String> local = new ThreadLocal<>();
+        local.set("Hello");
+        // Method nextHashCode = ThreadLocal.class.getDeclaredMethod("nextHashCode");
+        Method nextHashCode = local.getClass().getDeclaredMethod("nextHashCode");
+        nextHashCode.setAccessible(true);
+        int invoke1 = (int) nextHashCode.invoke(local);
+        int invoke2 = (int) nextHashCode.invoke(local);
+        int invoke3 = (int) nextHashCode.invoke(local);
+        int invoke4 = (int) nextHashCode.invoke(local);
+        System.out.println(invoke1);
+        System.out.println(invoke1 & 15);
+        System.out.println(invoke2);
+        System.out.println(invoke2 & 15);
+        System.out.println(invoke3);
+        System.out.println(invoke3 & 15);
+        System.out.println(invoke4);
+        System.out.println(invoke4 & 15);
+    }
+}
