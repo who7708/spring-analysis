3c31c13b17b73ddcb8ba90117284f4da91b2b55c
Chris <who7708@163.com>
2023-08-10 17:14:54 +0800

    基础测试

--- src/test/java/org/spring/boot/test/base/TestBase.java
+++ src/test/java/org/spring/boot/test/base/TestBase.java
@@ -6,4 +6,16 @@ package org.spring.boot.test.base;
  * @since 2023-08-07
  */
 public class TestBase {
+    public int compute() {
+        int a = 1;
+        int b = 2;
+        int c = (a + b) * 10;
+        return c;
+    }
+
+    public static void main(String[] args) {
+        TestBase test = new TestBase();
+        int result = test.compute();
+        System.out.println(result);
+    }
 }
