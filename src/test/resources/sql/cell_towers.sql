6f3cea0f176832107b4f33c338a8f3b654382767
Chris <who7708@163.com>
2023-09-08 01:18:10 +0800

    蜂窝信号塔 测试数据集2

--- src/test/resources/sql/cell_towers.sql
+++ src/test/resources/sql/cell_towers.sql
@@ -20,3 +20,20 @@ CREATE TABLE cell_towers
     updated       DATETIME,
     averageSignal TINYINT UNSIGNED
 );
+
+ALTER table cell_towers add INDEX idx_radio(radio);
+# 按类型划分的基站数量：
+EXPLAIN
+    SELECT radio, count(radio) AS c FROM cell_towers GROUP BY radio ORDER BY c DESC;
+
+ALTER table cell_towers add INDEX idx_mcc(mcc);
+# 各个移动国家代码（MCC）对应的蜂窝信号塔数量：
+EXPLAIN
+    SELECT mcc, count(mcc) as c FROM cell_towers GROUP BY mcc ORDER BY c DESC LIMIT 10;
+
+ALTER table cell_towers add INDEX  idx_radio_mcc(radio, mcc);
+
+ALTER table cell_towers DROP INDEX idx_radio;
+ALTER table cell_towers DROP INDEX idx_mcc;
+
+show INDEX FROM cell_towers;
