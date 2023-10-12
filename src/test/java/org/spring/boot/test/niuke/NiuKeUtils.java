ec81ed4d049ff4263e1960134b6661ec02ce7bac
Chris <who7708@163.com>
2023-10-12 18:52:40 +0800

    创建完全二叉树

--- src/test/java/org/spring/boot/test/niuke/NiuKeUtils.java
+++ src/test/java/org/spring/boot/test/niuke/NiuKeUtils.java
@@ -34,4 +34,48 @@ public class NiuKeUtils {
         }
         System.out.println();
     }
+
+    // {1,2,0,3,4}
+    // 创建完全二叉树
+    public static TreeNode createBinaryTree(int[] arr) {
+        return createBinaryTree(arr, 0);
+    }
+
+    public static TreeNode createBinaryTree(int[] arr, int index) {
+        TreeNode treeNode = null;
+        if (index < arr.length) {
+            treeNode = new TreeNode(arr[index]);
+            // 对于顺序存储的完全二叉树，如果某个节点的索引为index，其对应的左子树的索引为2*index+1，右子树为2*index+1
+            treeNode.left = createBinaryTree(arr, 2 * index + 1);
+            treeNode.right = createBinaryTree(arr, 2 * index + 2);
+        }
+        return treeNode;
+    }
+
+    public static void printTreeNode(TreeNode root, String pos) {
+        // if (root == null) {
+        //     System.out.println("root is null");
+        //     return;
+        // }
+        // if ("root".equals(pos)) {
+        //     System.out.println(pos + ".val = " + (root != null ? root.val : null));
+        // }
+        // if ("left".equals(pos)) {
+        //     System.out.println(pos + ".val = " + (root != null ? root.val : null));
+        // }
+        // if ("right".equals(pos)) {
+        //     System.out.println(pos + ".val = " + (root != null ? root.val : null));
+        // }
+        System.out.println(pos + ".val = " + (root != null ? root.val : null));
+        if (root != null) {
+            printTreeNode(root.left, "left");
+            printTreeNode(root.right, "right");
+        }
+        // if (root != null && root.left != null) {
+        //     printTreeNode(root.left, "left");
+        // }
+        // if (root != null && root.right != null) {
+        //     printTreeNode(root.right, "right");
+        // }
+    }
 }
