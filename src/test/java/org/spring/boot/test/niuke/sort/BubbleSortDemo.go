a1e925e20b613a4828e4fd6b9c80ca58c26c0629
Chris <who7708@163.com>
2023-10-13 17:57:35 +0800

    1. 冒泡排序 go 实现

---
+++ src/test/java/org/spring/boot/test/niuke/sort/BubbleSortDemo.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	arr := []int{-10, 9, 20, 3, 0, 15, 7}
+	fmt.Println(arr)
+	bubbleSort(arr)
+	fmt.Println(arr)
+}
+
+func bubbleSort(arr []int) {
+	length := len(arr)
+	for i := 0; i < length; i++ {
+		for j := i; j < length; j++ {
+			if arr[i] > arr[j] {
+				arr[i], arr[j] = arr[j], arr[i]
+			}
+		}
+	}
+}
