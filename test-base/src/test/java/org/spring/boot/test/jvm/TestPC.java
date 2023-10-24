c78cef9cf90dc51016f3dbea66e3bfd66869b87e
Chris <who7708@163.com>
2023-10-24 22:08:57 +0800

    pc计数器测试

---
+++ test-base/src/test/java/org/spring/boot/test/jvm/TestPC.java
@@ -0,0 +1,15 @@
+package org.spring.boot.test.jvm;
+
+/**
+ * @author Chris
+ * @version 1.0.0
+ * @since 2023/10/24
+ */
+public class TestPC {
+    public static void main(String[] args) {
+        int i = 1;
+        int j = 2;
+        int k = i + j;
+        System.out.println(k);
+    }
+}
