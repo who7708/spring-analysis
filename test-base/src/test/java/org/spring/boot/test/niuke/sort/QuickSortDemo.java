49ef20d2d8db15d08718989cddb63255c8b1b2b9
Chris <who7708@163.com>
2023-10-24 22:08:28 +0800

    快速排序优化

--- test-base/src/test/java/org/spring/boot/test/niuke/sort/QuickSortDemo.java
+++ test-base/src/test/java/org/spring/boot/test/niuke/sort/QuickSortDemo.java
@@ -29,31 +29,33 @@ public class QuickSortDemo {
 
     private int[] quickSort(int[] arr, int left, int right) {
         if (left < right) {
-            int partitionIndex = partition(arr, left, right);
-            quickSort(arr, left, partitionIndex - 1);
-            quickSort(arr, partitionIndex + 1, right);
+            int pivotIndex = partition(arr, left, right);
+            quickSort(arr, left, pivotIndex - 1);
+            quickSort(arr, pivotIndex + 1, right);
         }
         return arr;
     }
 
     private int partition(int[] arr, int left, int right) {
-        // 设定基准值（pivot）
-        int pivot = left;
-        int index = pivot + 1;
-        for (int i = index; i <= right; i++) {
-            if (arr[i] < arr[pivot]) {
-                swap(arr, i, index);
-                index++;
+        // 设定基准值（pivot），即中心点
+        int pivot = left + 1;
+        for (int i = pivot; i <= right; i++) {
+            if (arr[i] < arr[left]) {
+                swap(arr, i, pivot);
+                pivot++;
             }
         }
-        swap(arr, pivot, index - 1);
-        return index - 1;
+        swap(arr, left, pivot - 1);
+        return pivot - 1;
     }
 
     private void swap(int[] arr, int i, int j) {
         int temp = arr[i];
         arr[i] = arr[j];
         arr[j] = temp;
+        // arr[i] = arr[i] ^ arr[j];
+        // arr[j] = arr[i] ^ arr[j];
+        // arr[i] = arr[i] ^ arr[j];
     }
 
 }
