7abe3023b6f8c08a8f5e71a1b17dbbd99c116fcd
Chris <who7708@163.com>
2023-10-24 14:52:15 +0800

    算法题

---
+++ test-base/src/test/java/TestDemo.java
@@ -0,0 +1,51 @@
+/**
+ * @author Chris
+ * @version 1.0.0
+ * @since 2023/10/24
+ */
+public class TestDemo {
+
+    //    怎么样使用插入、删除、替换操作 使两个不同的单词转换成相同单词， 所需的最小操作数是多少？
+    //
+    // 1：
+    //
+    // 输入：c1 = "passenger", c2 = "flight"输出：8解释：passenger-> plight (将 'f' 替换为 'p')
+    // passenger-> paight (将 'l' 替换为 'a')
+    // passenger-> pasght (将 'i' 替换为 's')
+    // passenger-> passght (将 's' 插入)
+    // passenger-> passeght (将 'e' 插入)
+    // passenger-> passenght (将 'n' 插入)
+    // passenger-> passenget (将 'h' 替换为 'e)
+    // passenger-> passenger (将 't' 替换为 'r')
+    // 2：
+    //
+    // 输入：word1 = "hotel", word2 = "telephone"输出：7解释：hotel-> elephone (删除 't')
+    // hotel-> lephone (删除 'e')
+    // hotel-> ephone (删除 'l')
+    // hotel-> phone (删除 'e')
+    // hotel-> hone (删除 'p')hotel-> hote (将 'n' 替换为 't')hotel-> hotel (将 'l' 插入)
+    //
+    //
+    // 提示：
+    //
+    // 0 <= word1.length, word2.length <= 500
+    // word1 和 word2 由小写英文字母组成
+    // 每次只操作一个字母
+    // 每步插入、删除、替换都只算一次操作
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
