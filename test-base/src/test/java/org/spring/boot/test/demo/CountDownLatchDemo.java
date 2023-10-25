da455421576bec7890d7655899a5c6976c3bdddf
Chris <who7708@163.com>
2023-10-25 13:08:03 +0800

    update CountDownLatch 测试

--- test-base/src/test/java/org/spring/boot/test/demo/CountDownLatchDemo.java
+++ test-base/src/test/java/org/spring/boot/test/demo/CountDownLatchDemo.java
@@ -7,13 +7,14 @@ import java.util.concurrent.CountDownLatch;
  * */
 public class CountDownLatchDemo {
     public static void main(String[] args) throws Exception {
-        CountDownLatch countDownLatch = new CountDownLatch(6);
+        CountDownLatch countDownLatch = new CountDownLatch(2);
 
-        for (int i = 1; i <= 6; i++) {
-            new Thread(() -> {
+        for (int i = 1; i <= 2; i++) {
+            Thread thread = new Thread(() -> {
                 System.out.println(Thread.currentThread().getName() + "\t 上完自习，离开教室");
                 countDownLatch.countDown();
-            }, String.valueOf(i)).start();
+            }, String.valueOf(i));
+            thread.start();
         }
 
         countDownLatch.await();
