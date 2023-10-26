cc4e3720fbcfd9b924a80dc713136ad67bc1c8af
Chris <who7708@163.com>
2023-10-26 14:33:58 +0800

    逃逸分析，标题替换，锁消除

--- test-base/src/test/java/org/spring/boot/test/jvm/TestDoEscapeAnalysis.java
+++ test-base/src/test/java/org/spring/boot/test/jvm/TestDoEscapeAnalysis.java
@@ -14,6 +14,8 @@ public class TestDoEscapeAnalysis {
     // -Xmx64m -Xms64m -Xmn32m -XX:+DoEscapeAnalysis -XX:+PrintGCDetails -XX:+HeapDumpOnOutOfMemoryError
     //  -XX:+PrintEscapeAnalysis
     // 发生 MinorGC 后，在栈上分配的对象会被回收，即逃逸分析发生作用
+    // -XX:+EliminateAllocations 开启标量替换（JDK8默认开启）；
+    // -XX:+EliminateLocks 开启同步消除（JDK8默认开启）；
     public static void main(String[] args) throws InterruptedException {
 
         for (int i = 0; i < 1000000; i++) {
