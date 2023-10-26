dcfa60701ee19decdf130dcbe7838e4506168a0e
Chris <who7708@163.com>
2023-10-26 14:31:06 +0800

    优化逃逸分析测试，使用其生效

--- test-base/src/test/java/org/spring/boot/test/jvm/TestDoEscapeAnalysis.java
+++ test-base/src/test/java/org/spring/boot/test/jvm/TestDoEscapeAnalysis.java
@@ -11,8 +11,9 @@ import lombok.Data;
  * @since 2023/10/26
  */
 public class TestDoEscapeAnalysis {
-    // -Xmx2G -Xms2G -XX:+DoEscapeAnalysis -XX:+PrintGCDetails -XX:+HeapDumpOnOutOfMemoryError
+    // -Xmx64m -Xms64m -Xmn32m -XX:+DoEscapeAnalysis -XX:+PrintGCDetails -XX:+HeapDumpOnOutOfMemoryError
     //  -XX:+PrintEscapeAnalysis
+    // 发生 MinorGC 后，在栈上分配的对象会被回收，即逃逸分析发生作用
     public static void main(String[] args) throws InterruptedException {
 
         for (int i = 0; i < 1000000; i++) {
