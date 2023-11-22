bb251d3879ffeeb73c099493bcd179d7b9215049
Chris <who7708@163.com>
2023-11-22 16:33:29 +0800

    NC165 最长公共子序列(一) 代码优化

--- test-base/src/test/java/org/spring/boot/test/niuke/all/NiuKeNC165.java
+++ test-base/src/test/java/org/spring/boot/test/niuke/all/NiuKeNC165.java
@@ -46,10 +46,10 @@ public class NiuKeNC165 {
         for (int i = 1; i <= mLength; i++) {
             for (int j = 1; j <= nLength; j++) {
                 // if (c1[i - 1] != c2[j - 1]) {
-                if (s1.charAt(i - 1) != s2.charAt(j - 1)) {
-                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
-                } else {
+                if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
                     dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
                 }
             }
         }
