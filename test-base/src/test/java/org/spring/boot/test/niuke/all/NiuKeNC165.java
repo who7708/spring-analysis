68013ef2d4e05b39330f127bec6b698a7e65f77c
Chris <who7708@163.com>
2023-11-22 16:25:20 +0800

    NC165 最长公共子序列(一) 代码优化

--- test-base/src/test/java/org/spring/boot/test/niuke/all/NiuKeNC165.java
+++ test-base/src/test/java/org/spring/boot/test/niuke/all/NiuKeNC165.java
@@ -5,6 +5,10 @@ import org.junit.Test;
 /**
  * NC165 最长公共子序列(一)
  * <a href="https://www.nowcoder.com/practice/8cb175b803374e348a614e34b80ae191">...</a>
+ * 描述
+ * 给定两个字符串 s1 和 s2，长度为m和n 。求两个字符串最长公共子序列的长度。
+ * 所谓子序列，指一个字符串删掉部分字符（也可以不删）形成的字符串。例如：字符串 "arcaea" 的子序列有 "ara" 、 "rcaa" 等。但 "car" 、 "aaae" 则不是它的子序列。
+ * 所谓 s1 和 s2 的最长公共子序列，即一个最长的字符串，它既是 s1 的子序列，也是 s2 的子序列。
  *
  * @author Chris
  * @version 1.0.0
@@ -20,8 +24,6 @@ public class NiuKeNC165 {
     }
 
     /**
-     * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
-     *
      * s1和s2最长公共子序列的长度
      *
      * @param s1 string字符串
@@ -33,17 +35,18 @@ public class NiuKeNC165 {
         int mLength = s1.length();
         int nLength = s2.length();
         int[][] dp = new int[mLength + 1][nLength + 1];
-        char[] c1 = s1.toCharArray();
-        char[] c2 = s2.toCharArray();
-        for (int i = 0; i <= mLength; i++) {
-            dp[i][0] = 0;
-        }
-        for (int j = 0; j <= nLength; j++) {
-            dp[0][j] = 0;
-        }
+        // char[] c1 = s1.toCharArray();
+        // char[] c2 = s2.toCharArray();
+        // for (int i = 0; i <= mLength; i++) {
+        //     dp[i][0] = 0;
+        // }
+        // for (int j = 0; j <= nLength; j++) {
+        //     dp[0][j] = 0;
+        // }
         for (int i = 1; i <= mLength; i++) {
             for (int j = 1; j <= nLength; j++) {
-                if (c1[i - 1] != c2[j - 1]) {
+                // if (c1[i - 1] != c2[j - 1]) {
+                if (s1.charAt(i - 1) != s2.charAt(j - 1)) {
                     dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
                 } else {
                     dp[i][j] = dp[i - 1][j - 1] + 1;
