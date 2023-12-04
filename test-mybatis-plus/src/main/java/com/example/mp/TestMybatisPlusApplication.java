56ed3e1232155c2c174f55121df8b478c68d4e01
Chris <who7708@163.com>
2023-12-04 11:02:31 +0800

    Mybatis-Plus 测试

---
+++ test-mybatis-plus/src/main/java/com/example/mp/TestMybatisPlusApplication.java
@@ -0,0 +1,13 @@
+package com.example.mp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestMybatisPlusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestMybatisPlusApplication.class, args);
+    }
+
+}
---
+++ test-mybatis-plus/src/test/java/com/example/mp/TestMybatisPlusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestMybatisPlusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
