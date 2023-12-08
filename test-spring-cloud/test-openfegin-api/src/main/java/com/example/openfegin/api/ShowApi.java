7d59f32a907b8d52cb0a78c60b3974da7ab79e2c
Chris <who7708@163.com>
2023-12-09 02:08:06 +0800

    定义演出rpc接口

--- test-spring-cloud/test-openfegin-api/src/main/java/com/example/openfegin/api/ShowApi.java
+++ test-spring-cloud/test-openfegin-api/src/main/java/com/example/openfegin/api/ShowApi.java
@@ -1,6 +1,11 @@
 package com.example.openfegin.api;
 
+import com.example.openfegin.model.Show;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 /**
  * @author Chris
@@ -9,4 +14,15 @@ import org.springframework.cloud.openfeign.FeignClient;
  */
 @FeignClient("test-eureka-client-b")
 public interface ShowApi {
+    @RequestMapping("/show/list")
+    List<Show> showSessionList();
+
+    /**
+     * 根据主办方ID查询演出列表
+     *
+     * @param organizerId 主办方ID
+     * @return 演出列表
+     */
+    @RequestMapping("/show/list/{id}")
+    List<Show> showSessionList(@PathVariable("id") int organizerId);
 }
